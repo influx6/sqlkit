006ea63c9a76f425b826962c80c234fe6eb5661b
Alexander Ewetumo <trinoxf@gmail.com>
2017-12-15 17:33:35 +0100

    Update templates with migration and fix naming

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/gokit/sqlkit/sql"
+	"github.com/influx6/faux/context"
+	"github.com/influx6/faux/flags"
+	"github.com/influx6/faux/metrics"
+	"github.com/influx6/faux/metrics/custom"
+	"github.com/influx6/moz/ast"
+)
+
+func main() {
+	flags.Run("sqlkit", flags.Command{
+		Name:      "generate",
+		ShortDesc: "Generates mongo CRUD packages for structs",
+		Desc:      "Generates from go packages to create CRUD implementations for types using mongodb",
+		Action: func(ctx context.Context) error {
+			force, _ := ctx.Bag().GetBool("force")
+			dest, _ := ctx.Bag().GetString("dest")
+			verbose, _ := ctx.Bag().GetBool("verbose")
+
+			logs := metrics.New()
+			if verbose {
+				logs = metrics.New(custom.StackDisplay(os.Stderr))
+			}
+
+			currentdir, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+
+			generators := ast.NewAnnotationRegistryWith(logs)
+			generators.Register("sqlapi", sql.APIGen)
+
+			res, err := ast.ParseAnnotations(logs, currentdir)
+			if err != nil {
+				return err
+			}
+
+			return ast.SimplyParse(dest, logs, generators, force, res...)
+		},
+		Flags: []flags.Flag{
+			&flags.BoolFlag{
+				Name: "verbose",
+				Desc: "verbose logs all operations out to console.",
+			},
+			&flags.BoolFlag{
+				Name: "force",
+				Desc: "force regeneration of packages annotation directives.",
+			},
+			&flags.StringFlag{
+				Name:    "dest",
+				Default: "./",
+				Desc:    "relative destination for package",
+				Validation: func(received string) error {
+					if filepath.IsAbs(received) {
+						return errors.New("only relative paths not absolute allowed")
+					}
+					return nil
+				},
+			},
+		},
+	})
+
+}
---
+++ sql/sql.go
@@ -0,0 +1,228 @@
+package sql
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/gokit/sqlkit/static"
+	"github.com/influx6/faux/fmtwriter"
+	"github.com/influx6/moz/ast"
+	"github.com/influx6/moz/gen"
+)
+
+// APIGen implements the necessary logic to generated CRUD structures for using a sql based database.
+func APIGen(toDir string, an ast.AnnotationDeclaration, str ast.StructDeclaration, pkgDeclr ast.PackageDeclaration, pkg ast.Package) ([]gen.WriteDirective, error) {
+	updateAction := str
+	createAction := str
+
+	if len(str.Associations) != 0 {
+		if newAction, ok := str.Associations["New"]; ok {
+			if action, err := ast.FindStructType(pkgDeclr, newAction.TypeName); err == nil && action.Declr != nil {
+				createAction = action
+			}
+		}
+
+		if upAction, ok := str.Associations["Update"]; ok {
+			if action, err := ast.FindStructType(pkgDeclr, upAction.TypeName); err == nil && action.Declr != nil {
+				updateAction = action
+			}
+		}
+	}
+
+	packageName := fmt.Sprintf("%ssql", strings.ToLower(str.Object.Name.Name))
+	sqlTestGen := gen.Block(
+		gen.Package(
+			gen.Name(fmt.Sprintf("%s_test", packageName)),
+			gen.Imports(
+				gen.Import("os", ""),
+				gen.Import("testing", ""),
+				gen.Import("encoding/json", ""),
+				gen.Import("github.com/influx6/faux/db", ""),
+				gen.Import("github.com/influx6/faux/tests", ""),
+				gen.Import("github.com/influx6/faux/db/sql", ""),
+				gen.Import("github.com/influx6/faux/metrics", ""),
+				gen.Import("github.com/influx6/faux/context", ""),
+				gen.Import("github.com/influx6/faux/metrics/custom", ""),
+				gen.Import("github.com/go-sql-driver/mysql", "_"),
+				gen.Import("github.com/lib/pq", "_"),
+				gen.Import("github.com/mattn/go-sqlite3", "_"),
+				gen.Import(filepath.Join(str.Path, toDir, packageName), "sqldb"),
+				gen.Import(str.Path, ""),
+			),
+			gen.Block(
+				gen.SourceTextWith(
+					string(static.MustReadFile("sql-api-test.tml", true)),
+					gen.ToTemplateFuncs(
+						ast.ASTTemplatFuncs,
+						template.FuncMap{
+							"hasFunc": ast.HasFunctionFor(pkgDeclr),
+						},
+					),
+					struct {
+						Pkg          *ast.PackageDeclaration
+						Struct       ast.StructDeclaration
+						CreateAction ast.StructDeclaration
+						UpdateAction ast.StructDeclaration
+					}{
+						Pkg:          &pkgDeclr,
+						Struct:       str,
+						CreateAction: createAction,
+						UpdateAction: updateAction,
+					},
+				),
+			),
+		),
+	)
+
+	sqlReadmeGen := gen.Block(
+		gen.Block(
+			gen.SourceText(
+				string(static.MustReadFile("sql-api-readme.tml", true)),
+				struct {
+					Pkg          *ast.PackageDeclaration
+					Struct       ast.StructDeclaration
+					CreateAction ast.StructDeclaration
+					UpdateAction ast.StructDeclaration
+				}{
+					Pkg:          &pkgDeclr,
+					Struct:       str,
+					CreateAction: createAction,
+					UpdateAction: updateAction,
+				},
+			),
+		),
+	)
+
+	sqlJSONGen := gen.Block(
+		gen.Package(
+			gen.Name(fmt.Sprintf("%s_test", packageName)),
+			gen.Imports(
+				gen.Import("encoding/json", ""),
+				gen.Import(str.Path, ""),
+			),
+			gen.Block(
+				gen.SourceTextWith(
+					string(static.MustReadFile("sql-api-json.tml", true)),
+					template.FuncMap{
+						"map":       ast.MapOutFields,
+						"mapValues": ast.MapOutValues,
+						"mapJSON":   ast.MapOutFieldsToJSON,
+						"hasFunc":   ast.HasFunctionFor(pkgDeclr),
+					},
+					struct {
+						Pkg          *ast.PackageDeclaration
+						Struct       ast.StructDeclaration
+						CreateAction ast.StructDeclaration
+						UpdateAction ast.StructDeclaration
+					}{
+						Pkg:          &pkgDeclr,
+						Struct:       str,
+						CreateAction: createAction,
+						UpdateAction: updateAction,
+					},
+				),
+			),
+		),
+	)
+
+	// sqlMigrationsGen := gen.Block(
+	// 	gen.Package(
+	// 		gen.Name(packageName),
+	// 	),
+	// 	gen.Block(
+	// 		gen.SourceTextWith(
+	// 			string(static.MustReadFile("sql-api-migration.tml", true)),
+	// 			gen.ToTemplateFuncs(
+	// 				ast.ASTTemplatFuncs,
+	// 				template.FuncMap{
+	// 					"hasFunc": ast.HasFunctionFor(pkgDeclr),
+	// 				},
+	// 			),
+	// 			struct {
+	// 				Struct       ast.StructDeclaration
+	// 				CreateAction ast.StructDeclaration
+	// 				UpdateAction ast.StructDeclaration
+	// 				Pkg          *ast.PackageDeclaration
+	// 			}{
+	// 				Struct:       str,
+	// 				Pkg:          &pkgDeclr,
+	// 				CreateAction: createAction,
+	// 				UpdateAction: updateAction,
+	// 			},
+	// 		),
+	// 	),
+	// )
+
+	sqlGen := gen.Block(
+		gen.Commentary(
+			gen.SourceText(`Package `+packageName+` provides a auto-generated package which contains a sql CRUD API for the specific {{.Object.Name}} struct in package {{.Package}}.`, str),
+		),
+		gen.Package(
+			gen.Name(packageName),
+			gen.Imports(
+				gen.Import("fmt", ""),
+				gen.Import("bytes", ""),
+				gen.Import("strings", ""),
+				gen.Import("encoding/json", ""),
+				gen.Import("github.com/influx6/faux/db", ""),
+				gen.Import("github.com/influx6/faux/db/sql", ""),
+				gen.Import("github.com/influx6/faux/context", ""),
+				gen.Import("github.com/influx6/faux/metrics", ""),
+				gen.Import(str.Path, ""),
+			),
+			gen.Block(
+				gen.SourceTextWith(
+					string(static.MustReadFile("sql-api.tml", true)),
+					gen.ToTemplateFuncs(
+						ast.ASTTemplatFuncs,
+						template.FuncMap{
+							"hasFunc": ast.HasFunctionFor(pkgDeclr),
+						},
+					),
+					struct {
+						Pkg          *ast.PackageDeclaration
+						Struct       ast.StructDeclaration
+						CreateAction ast.StructDeclaration
+						UpdateAction ast.StructDeclaration
+					}{
+						Pkg:          &pkgDeclr,
+						Struct:       str,
+						CreateAction: createAction,
+						UpdateAction: updateAction,
+					},
+				),
+			),
+		),
+	)
+
+	return []gen.WriteDirective{
+		{
+			Writer:   sqlJSONGen,
+			FileName: fmt.Sprintf("%s_fixtures_test.go", packageName),
+			Dir:      packageName,
+		},
+		{
+			Writer:   sqlReadmeGen,
+			FileName: "README.md",
+			Dir:      packageName,
+		},
+		{
+			Writer:   fmtwriter.New(sqlTestGen, true, true),
+			FileName: fmt.Sprintf("%s_test.go", packageName),
+			Dir:      packageName,
+		},
+		{
+			Writer:   fmtwriter.New(sqlGen, true, true),
+			FileName: fmt.Sprintf("%s.go", packageName),
+			Dir:      packageName,
+		},
+		// {
+		// 	DontOverride: true,
+		// 	Writer:       fmtwriter.New(sqlMigrationsGen, true, true),
+		// 	FileName:     fmt.Sprintf("%s_migration.go", packageName),
+		// 	Dir:          packageName,
+		// },
+	}, nil
+}
